bf74c720e0702482faa840022206572ac6241b6c
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2025-05-21 14:23:28 +0200

    more experimentation

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 First draft of an experimental RDF conversion tool, parameters may change heavily.
 Contributions and opinions welcome.
 
-To help users coming from the [Raptor RDF utility](https://github.com/dajobe/raptor) and to minimize bikeshedding,
-many of the parameter names, values and descriptions are copied from rapper, but I may differ in others.
-As in rapper, the converted file is written to stdout.
+To help users coming from the [Raptor RDF utility](https://github.com/dajobe/raptor) and [oxigraph-cli](https://github.com/oxigraph/oxigraph), and to minimize bikeshedding,
+many of the parameter names, values and descriptions are copied, but I may differ in others.
+As in rapper, the converted file is written to stdout by default.
 Rapper is Copyright (C) David Beckett from the University of Bristol.
 Heavy reliance on the [Sophia](https://github.com/pchampin/sophia_rs) library.
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
+#![allow(unused_imports)]
 use clap::{Parser, Subcommand};
 use color_eyre::config::HookBuilder;
 use color_eyre::eyre::{Report, WrapErr};
-//use hdt::{Hdt, HdtGraph};
-//use log::info;
-use sophia::api::prelude::{Stringifier, TripleSerializer};
-use sophia::turtle::serializer::nt::NtSerializer;
-use sophia::turtle::serializer::turtle::{TurtleConfig, TurtleSerializer};
+use hdt::{Hdt, HdtGraph};
+//use sophia::api::prelude::{Triple, TripleSource};
+use serde::Serialize;
 //use sophia::turtle::parser::{nt, turtle};
 use sophia::api::graph::Graph;
 use sophia::api::prelude::TripleSource;
-//use sophia::api::prelude::{Triple, TripleSource};
-use serde::Serialize;
-use sophia::inmem::graph::FastGraph;
-use sophia::turtle::parser::turtle;
+//use hdt::{Hdt, HdtGraph};
+//use log::info;
+use sophia::api::prelude::{Stringifier, TripleSerializer};
+use sophia::api::term::matcher::{Any, TermMatcher};
+// LightGraph loads 1M triples in 12s, LightGraph takes 18s to load but queries faster
+use sophia::inmem::graph::LightGraph;
+use sophia::turtle::parser::{nt, turtle};
+use sophia::turtle::serializer::nt::NtSerializer;
+use sophia::turtle::serializer::turtle::{TurtleConfig, TurtleSerializer};
+use std::convert::Infallible;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::PathBuf;
 //use std::io::{BufReader, stdin};
-
-/*enum Format {
-    NTriples,
-    RdfXml,
-    Turtle,
-}*/
 
 //#[derive(Parser, Debug)]
 #[derive(Parser)]
@@ -52,12 +52,13 @@ struct Cli {
 
 #[derive(Debug, Clone, clap::ValueEnum, Default, Serialize)]
 #[serde(rename_all = "kebab-case")]
-enum Serializer {
+enum Format {
     /// RDF/XML
     Rdfxml,
     /// Turtle Terse RDF Triple Language
     Turtle,
     Jsonld,
+    Hdt,
     /// N-Triples
     #[default]
     Ntriples,
@@ -66,26 +67,30 @@ enum Serializer {
 #[derive(Subcommand)]
 enum Command {
     Convert {
-        input_uri: String,
+        input_uri: PathBuf,
         /// Set the input format.
         #[arg(short, long)]
-        input: Option<String>,
+        input: Format,
         /// Set the output format.
         #[arg(short, long)]
-        output: Serializer,
+        output: Format,
     },
     Info {
         //#[arg(short, long)]
         //count: u8,
-        filename: String,
+        filename: PathBuf,
+    },
+    Test {
+        filename: PathBuf,
     },
     Panic,
 }
 
+/// Load RDF graph from the RDF Turtle file specified in the config.
 //fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn main() -> Result<(), Report> {
     use Command::*;
-    use Serializer::*;
+    //use Format::*;
     let cli = Cli::parse();
     HookBuilder::default()
         .panic_section("consider reporting the bug at https://github.com/KonradHoeffner/rdf/issues")
@@ -94,40 +99,40 @@ fn main() -> Result<(), Report> {
     match cli.command {
         Panic => panic!("I'm panicking"),
         Info { filename } => {
-            let file = File::open(filename.clone())
-                .wrap_err_with(|| format!("Error opening input file {}", filename))?;
+            let file = File::open(filename.clone()).wrap_err_with(|| format!("Error opening input file {filename:?}"))?;
             let br = BufReader::new(file);
             let triples = turtle::parse_bufread(br).collect_triples();
-            let graph: FastGraph = triples?;
+            let graph: LightGraph = triples?;
             println!("~ {} triples", graph.triples().size_hint().0);
         }
-        Convert {
-            input_uri,
-            input,
-            output,
-        } => {
+        Test { filename } => {
+            let file = File::open(filename.clone()).wrap_err_with(|| format!("Error opening input file {filename:?}"))?;
+            Hdt::new(std::io::BufReader::new(file)).expect(&format!("Error loading HDT from {filename:?}"));
+            //.wrap_err_with(|| format!("Error loading HDT from {filename:?}"))?
+        }
+        Convert { input_uri, input, output } => {
             if !cli.quiet {
-                eprintln!("rdf: Parsing URI {} with parser turtle", input_uri);
+                eprintln!("rdf: Parsing URI {input_uri:?} with parser {input:?}");
             }
-            let file = File::open(input_uri.clone())
-                .wrap_err_with(|| format!("Error opening input URI {}", input_uri))?;
+            let file = File::open(input_uri.clone()).wrap_err_with(|| format!("Error opening input URI {input_uri:?}"))?;
             let br = BufReader::new(file);
-            let triples = turtle::parse_bufread(br).collect_triples();
-            let graph: FastGraph = triples?;
+            let triples = match input {
+                Format::Turtle => turtle::parse_bufread(br).collect_triples(),
+                Format::Ntriples => nt::parse_bufread(br).collect_triples(),
+                //Hdt => nt::parse_bufread(br).collect_triples(),
+                _ => todo!("parsing {:?} not implemented", input),
+            };
+            let graph: LightGraph = triples?;
             if !cli.quiet {
                 eprintln!("rdf: Serializing with serializer {output:?}");
             }
             match output {
-                Ntriples => {
-                    println!(
-                        "{}",
-                        NtSerializer::new_stringifier()
-                            .serialize_graph(&graph)?
-                            .to_string()
-                    );
+                Format::Ntriples => {
+                    println!("{}", NtSerializer::new_stringifier().serialize_graph(&graph)?.to_string());
                 }
-                Turtle => {
-                    let config = TurtleConfig::new().with_pretty(true);
+                Format::Turtle => {
+                    let config = TurtleConfig::new().with_pretty(false); // pretty printing is much slower
+
                     //.with_own_prefix_map(prefixes().clone());
                     println!(
                         "{}",
@@ -142,10 +147,7 @@ fn main() -> Result<(), Report> {
                 }
             }
             if !cli.quiet {
-                eprintln!(
-                    "rdf: Converting returned {} triples",
-                    graph.triples().size_hint().0
-                );
+                eprintln!("rdf: Converting returned {} triples", graph.triples().size_hint().0);
             }
         }
     }
